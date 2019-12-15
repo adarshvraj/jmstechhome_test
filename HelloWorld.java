2f3e794d82e7d4047495d11d292020e107aae0a2
Adarsh Raj <learningpro.it@gmail.com>
2019-12-15 07:44:59 +0530

    The nation and state added

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     System.out.println("Hello, India");
     System.out.println("Hello, Bharath");
     System.out.println("Hello, Kerala");
+    System.out.println("Hello, nation and the state");
   }
 }
